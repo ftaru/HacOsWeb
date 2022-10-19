ff1f5705de176cdef9c18560ad60fbe4784448ad
ftaru <klokentime2014@gmail.com>
2022-10-20 03:26:49 +0500

    feat: add full dark theme support

--- scripts/changeDOM.js
+++ scripts/changeDOM.js
@@ -23,24 +23,3 @@ function addBuyButton() {
     newDiv2.onclick = function() { congrats(); };
     newDiv.appendChild(newDiv2);
 }
-
-function changeTheme(){
-    var defaultTheme = "rgb(0, 128, 128)"
-    var darkTheme = "rgb(111, 115, 120)";
-    var mdDark = "rgb(38, 50, 56)";
-    var mdLightGray = "rgb(176, 190, 197)";
-    var mdMidGray = "rgb(55, 71, 79)";
-
-    var currentCSSTheme = window.getComputedStyle(document.body, null).backgroundColor;
-    const defaultBgColor = "silver";
-
-    if (currentCSSTheme !== defaultTheme) {
-        document.body.style.backgroundColor = defaultTheme;  
-        document.getElementById('navbarId').style.background = defaultBgColor; 
-        document.getElementById('buyWindowContent').style.background = defaultBgColor;
-    } else {
-        document.body.style.backgroundColor = mdMidGray;
-        document.getElementById('navbarId').style.background = mdDark;
-        document.getElementById('buyWindowContent').style.background = mdLightGray;
-    }
-}
